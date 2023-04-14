fd5aa13ec738cf756f43fff2c263673047c26431
Terry Lamb <terrylamb@gmail.com>
2023-04-14 10:59:15 -0400

    Add new answers

--- oop.rb
+++ oop.rb
@@ -48,6 +48,33 @@ p vamp1.drink
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+    attr_reader :name, :rider, :color, :is_hungry, :eaten
+    def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+        @eaten = 0
+    end
+    def eat
+        @eaten += 1
+        if @eaten >= 4
+            @is_hungry = false
+        end 
+    end
+end
+
+
+lizard = Dragon.new("Drogon", "Khaleesi", "black")
+p lizard
+lizard.eat
+lizard.eat
+lizard.eat
+p lizard.is_hungry
+lizard.eat
+p lizard.is_hungry
+
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -57,3 +84,29 @@ p vamp1.drink
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+    attr_reader :name, :disposition, :age, :is_adult, :is_old, :has_ring
+    def initialize(name, disposition, age)
+        @name = name
+        @disposition = disposition
+        @age = 0
+        @is_adult = false
+        @is_old = false
+        @has_ring = false
+    end
+    def celebrate_birthday
+        @age += 1
+        if @age >= 33 && @age < 101
+            @is_adult = true
+        elsif @age >= 101
+            @is_old = true
+        end
+    end
+end
+
+person = Hobbit.new("Frodo", "Happy", 18)
+p person.celebrate_birthday
+
+
+
