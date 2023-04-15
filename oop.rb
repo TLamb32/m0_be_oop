43fea2320e82cd1a0c94de155f8827ce741e4ec8
Terry Lamb <terrylamb@gmail.com>
2023-04-14 20:33:44 -0400

    Add finishing touches on frodo

--- oop.rb
+++ oop.rb
@@ -24,21 +24,21 @@
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
-class Vampire
-    attr_reader :name, :pet, :thirsty
-    def initialize(name, pet = "bat")
-        @name = name
-        @pet = pet
-        @thirsty = true
-    end
+# class Vampire
+#     attr_reader :name, :pet, :thirsty
+#     def initialize(name, pet = "bat")
+#         @name = name
+#         @pet = pet
+#         @thirsty = true
+#     end
 
-    def drink
-        @thirsty == false
-    end
-end
+#     def drink
+#         @thirsty == false
+#     end
+# end
 
-p vamp1 = Vampire.new("Bob", "rat")
-p vamp1.drink
+# p vamp1 = Vampire.new("Bob", "rat")
+# p vamp1.drink
 
 
 #  Write a Dragon class
@@ -48,32 +48,32 @@ p vamp1.drink
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
-class Dragon
-    attr_reader :name, :rider, :color, :is_hungry, :eaten
-    def initialize(name, rider, color)
-        @name = name
-        @rider = rider
-        @color = color
-        @is_hungry = true
-        @eaten = 0
-    end
-    def eat
-        @eaten += 1
-        if @eaten >= 4
-            @is_hungry = false
-        end 
-    end
-end
+# class Dragon
+#     attr_reader :name, :rider, :color, :is_hungry, :eaten
+#     def initialize(name, rider, color)
+#         @name = name
+#         @rider = rider
+#         @color = color
+#         @is_hungry = true
+#         @eaten = 0
+#     end
+#     def eat
+#         @eaten += 1
+#         if @eaten >= 4
+#             @is_hungry = false
+#         end 
+#     end
+# end
 
 
-lizard = Dragon.new("Drogon", "Khaleesi", "black")
-p lizard
-lizard.eat
-lizard.eat
-lizard.eat
-p lizard.is_hungry
-lizard.eat
-p lizard.is_hungry
+# lizard = Dragon.new("Drogon", "Khaleesi", "black")
+# p lizard
+# lizard.eat
+# lizard.eat
+# lizard.eat
+# p lizard.is_hungry
+# lizard.eat
+# p lizard.is_hungry
 
 
 #  Write a Hobbit class
@@ -87,13 +87,22 @@ p lizard.is_hungry
 
 class Hobbit
     attr_reader :name, :disposition, :age, :is_adult, :is_old, :has_ring
-    def initialize(name, disposition, age)
+    def initialize(name, disposition, age = 0)
         @name = name
         @disposition = disposition
-        @age = 0
-        @is_adult = false
-        @is_old = false
-        @has_ring = false
+        @age = age
+        @is_adult = is_adult
+        @is_old = is_old
+        @has_ring = has_ring
+    end
+    def has_ring 
+        @name == "Frodo"
+    end
+    def is_adult
+        @age >= 33
+    end
+    def is_old
+        @age >= 101
     end
     def celebrate_birthday
         @age += 1
@@ -106,7 +115,8 @@ class Hobbit
 end
 
 person = Hobbit.new("Frodo", "Happy", 18)
-p person.celebrate_birthday
+120.times{person.celebrate_birthday}
 
+hobbit = Hobbit.new("Frodo", "Sad", 130)
 
-
+p hobbit
